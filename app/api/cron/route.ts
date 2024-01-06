1723641d3d0ad68de513029f7e82569e710d0ff0
Akshat Kumar <akshatkumar10203@gmail.com>
2024-01-06 20:46:56 +0530

    minor change

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -59,7 +59,7 @@ export async function GET(request: Request) {
           const productInfo = {
             title: updatedProduct.title,
             url: updatedProduct.url,
-            image: updatedProduct.imaged
+            image: updatedProduct.image
           };
           // Construct emailContent
           const emailContent = await generateEmailBody(productInfo, emailNotifType);
